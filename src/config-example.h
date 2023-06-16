edec592424dd31e9cd8e44e091c149448bf21376
Ingo Hinterding <ingo@hinterding.de>
2023-06-16 20:06:31 +0200

    init

---
+++ src/config-example.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "YOUR SSID";
+const char* password = "YOUR WIFI PASSWORD";
+const char* server = "YOUR URL"; // Server URL
+const char* token = "YOUR BEARER TOKEN";
+const char* endpoint = "PATH/TO/ENDPOINT";
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,227 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+#include "config.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFiClientSecure.h>
+
+Adafruit_MPU6050 mpu;
+
+char myID[] = "A";
+const int debug = 0;
+int last_side = 0;
+int movement_counter = 0;
+
+const int BUTTON_PIN = 0;
+bool buttonState = false;
+bool lastButtonState = false;
+
+#define LED_PIN 25
+
+WiFiClientSecure client;
+
+void sendValueToServer(const String &value)
+{
+
+  Serial.println("\nConnecting to " + String(server));
+  client.setInsecure(); // skip verification
+
+  if (!client.connect(server, 443))
+    Serial.println("Connection failed.");
+  else
+  {
+    Serial.println("Connected.");
+
+    client.println("POST " + String(endpoint) + " HTTP/1.1");
+    client.println("Host: " + String(server));
+    client.println("Connection: close");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Authorization: Bearer " + String(token));
+    client.print("Content-Length: ");
+    client.println(String(strlen("shortcode=XX")));
+    client.println();
+    client.print("shortcode=");
+    client.println(value);
+
+    while (client.connected())
+    {
+      String line = client.readStringUntil('\n');
+      if (line == "\r")
+      {
+        Serial.println("headers received");
+        break;
+      }
+    }
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available())
+    {
+      char c = client.read();
+      Serial.write(c);
+    }
+
+    client.stop();
+  }
+}
+
+void blinkLED(int numBlinks)
+{
+  pinMode(LED_PIN, OUTPUT);
+
+  for (int i = 0; i < numBlinks; i++)
+  {
+    digitalWrite(LED_PIN, HIGH); // LED einschalten
+    delay(500);                  // 500 Millisekunden warten
+    digitalWrite(LED_PIN, LOW);  // LED ausschalten
+    delay(500);                  // 500 Millisekunden warten
+  }
+}
+
+void startWifi()
+{
+  // Verbindung zum Wi-Fi-Netzwerk herstellen
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected.");
+}
+
+void stopWifi()
+{
+  // Wi-Fi-Verbindung trennen
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  Serial.println("Wi-Fi-Verbindung getrennt.");
+}
+
+void measure()
+{
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  if (debug == 1)
+  {
+    /* Print out the values */
+    Serial.print("X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println("");
+  }
+
+  /*
+   *  Sides (like on a real 1-6 die)
+   *  1 = z+
+   *  2 = x-
+   *  3 = y-
+   *  4 = y+
+   *  5 = x+
+   *  6 = z-
+   *
+   */
+
+  int side = 0;
+  if (a.acceleration.x >= +8)
+    side = 5;
+  if (a.acceleration.x <= -8)
+    side = 2;
+  if (a.acceleration.y >= +8)
+    side = 4;
+  if (a.acceleration.y <= -8)
+    side = 3;
+  if (a.acceleration.z >= +8)
+    side = 1;
+  if (a.acceleration.z <= -8)
+    side = 6;
+
+  if (side != last_side && side != 0)
+  {
+    last_side = side;
+    movement_counter = 0;
+    Serial.print("Seite: ");
+    Serial.println(side);
+  }
+
+  if (side == last_side && side != 0)
+  {
+    movement_counter++;
+  }
+
+  if (movement_counter == 2)
+  {
+    movement_counter++;
+    char roll[10];
+    sprintf(roll, "%c%d", myID[0], side);
+    Serial.println("....sending data...");
+    Serial.print(roll);
+    Serial.println("");
+
+    sendValueToServer(roll);
+  }
+}
+
+void change_id()
+{
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == LOW && lastButtonState == HIGH)
+  {
+    if (strcmp(myID, "A") == 0)
+    {
+      strcpy(myID, "B");
+      blinkLED(2);
+    }
+    else if (strcmp(myID, "B") == 0)
+    {
+      strcpy(myID, "C");
+      blinkLED(3);
+    }
+    else if (strcmp(myID, "B") == 0)
+    {
+      strcpy(myID, "A");
+      blinkLED(1);
+    }
+    Serial.print("ID: ");
+    Serial.println(myID);
+  }
+  lastButtonState = buttonState;
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+
+  Serial.println("--------------------");
+
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+  }
+  else
+  {
+    Serial.println("MPU6050 Found!");
+  }
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  startWifi();
+}
+
+void loop()
+{
+  change_id();
+  measure();
+  delay(1000);
+}
