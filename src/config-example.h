493a93e99fe43ed801c039d2348effc5d8e8ceb5
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-09-18 12:31:00 +0200

    docs: add required config

--- src/config-example.h
+++ src/config-example.h
@@ -1,11 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* ssid = "YOUR SSID";
-const char* password = "YOUR WIFI PASSWORD";
-const char* server = "YOUR URL"; // Server URL
-const int port = 3000; // insert your port
-const char* token = "YOUR BEARER TOKEN";
-const char* endpoint = "PATH/TO/ENDPOINT";
+const char *ssid = "YOUR SSID";
+const char *password = "YOUR WIFI PASSWORD";
+const char *server = "YOUR URL"; // Server URL
+const int port = 3000;           // insert your port
+const char *token = "YOUR BEARER TOKEN";
+const char *endpoint = "PATH/TO/ENDPOINT";
+const char myID[] = "A"; // One of A, B, C
 
 #endif
