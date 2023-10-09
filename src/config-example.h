c1bd17dcf11a2aa24b626ee1f45ab8a1d61b53f9
Raphael Arce <raphael.arce@ts.berlin>
2023-10-09 11:46:54 +0200

    fix: added missing http request header
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/config-example.h
+++ src/config-example.h
@@ -1,12 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char *ssid = "YOUR SSID";
-const char *password = "YOUR WIFI PASSWORD";
-const char *server = "YOUR URL"; // Server URL
-const int port = 3000;           // insert your port
-const char *token = "YOUR BEARER TOKEN";
-const char *endpoint = "PATH/TO/ENDPOINT";
-const char myID[] = "A"; // One of A, B, C
+const char myID[] = "A"; // "A", "B" or "C"
 
 #endif
